596eff7f64b08ca0df67d8f277de28ba3feb2074
Technical Dada <32122182+technicaldada@users.noreply.github.com>
2019-10-06 13:34:51 +0530

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "###############################################"
+echo -e "\e[36m###############################################\e[0m"
 toilet -f smblock --filter border:metal "WeaTher Forecast"
-echo "###############################################"
+echo -e "\e[36m###############################################\e[0m"
 echo -e "\e[1;31;42m ############ ©Technical Dada ###############\e[0m"
 echo -e "\e[1;31mUse Landscape For Mobile Devices\e[0m"
-echo "###########################################"
+echo -e "\e[36m###############################################\e[0m"
 echo -e "\e[1;33;4;44m Enter the name of the city you live or near city\e[0m"
 read town
 curl wttr.in/$town
