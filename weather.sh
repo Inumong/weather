6566ac3c29ece1ed746c0abf4452db2b7329ea60
Technical Dada <32122182+technicaldada@users.noreply.github.com>
2019-10-06 12:56:41 +0530

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo "##############################################"
-echo "## WEATHER FORECAST ##############"
-echo "## © Technical Dada ###############"
-echo "## Use Landscape for a good view ###"
+echo "###############################################"
+echo -e "\e[34m __      __       _____ _             \e[0m"
+echo -e "\e[34m \ \    / /__ __ |_   _| |_  ___ _ _  \e[0m"
+echo -e "\e[34m  \ \/\/ / -_) _` || | | ' \/ -_) '_| \e[0m"
+echo -e "\e[34m  _\_/\_/\___\__,_||_| |_||_\___|_|   \e[0m"
+echo -e "\e[34m | __|__ _ _ ___ __ __ _ __| |_       \e[0m"
+echo -e "\e[34m | _/ _ \ '_/ -_) _/ _` (_-<  _|      \e[0m"
+echo -e "\e[34m |_|\___/_| \___\__\__,_/__/\__|      \e[0m"
+echo "###############################################"
+echo -e "\e[1;31;42m #############© Technical Dada ###############\e[0m"
+echo -e "\e[31mUse Landscape For Mobile Devices\e[0m"
 echo "###########################################"
-echo "Enter the name of the city you live or near city"
+echo -e "\e[1;33;4;44m Enter the name of the city you live or near city\e[0m"
 read town
 curl wttr.in/$town
-echo "Also visit kalilinux.in for hacking tutorials"
+echo -e "\e[34mAlso visit kalilinux.in for hacking tutorials\e[0m"
