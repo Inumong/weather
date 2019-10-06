152963c513c9827fec1c259e9c1cb628369da322
Technical Dada <32122182+technicaldada@users.noreply.github.com>
2019-10-06 13:43:12 +0530

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -2,7 +2,7 @@
 echo -e "\e[36m###############################################\e[0m"
 toilet -f smblock --filter border:metal "WeaTher Forecast"
 echo -e "\e[36m###############################################\e[0m"
-echo -e "\e[1;31;42m ############ ©Technical Dada ###############\e[0m"
+echo -e "\e[1;31;42m ############ ©Technical Dada ############\e[0m"
 echo -e "\e[1;31mUse Landscape For Mobile Devices\e[0m"
 echo -e "\e[36m###############################################\e[0m"
 echo -e "\e[1;33;4;44m Enter the name of the city you live or near city\e[0m"
