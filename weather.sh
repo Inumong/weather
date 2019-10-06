52cd19c01b27b7c346e06f565d9253dcab0da132
Technical Dada <32122182+technicaldada@users.noreply.github.com>
2019-10-06 13:20:08 +0530

    Update weather.sh

--- weather.sh
+++ weather.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "###############################################"
-toilet -f smblock --filter border:metal "Weather Forecast"
+toilet -f smblock --filter border:metal "WeaTher Forecast"
 echo "###############################################"
 echo -e "\e[1;31;42m #############© Technical Dada ###############\e[0m"
 echo -e "\e[31mUse Landscape For Mobile Devices\e[0m"
