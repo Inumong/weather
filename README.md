da016e17430ca61c001e4b0b93d9f707134188e2
TheSpeedX <ggspeedx29@gmail.com>
2019-10-07 09:40:34 +0530

    Upgraded and typos

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Weather
-This is very simple bash script. This will run in any linux environment including Termux. ##
+This is very simple bash script. This will run in any linux environment including Termux.
+
 ## Installation
-git clone https://guthub.com/technicaldada/weather
+```
+git clone https://github.com/technicaldada/weather
 <br>
 cd weather
 <br>
 chmod +x weather.sh
 <br>
-./weather.sh
+./weather.sh```
+
+## QuickLook
 <img src="weather.png">
--- weather.sh
+++ weather.sh
@@ -7,5 +7,17 @@ echo -e "\e[1;31mUse Landscape For Mobile Devices\e[0m"
 echo -e "\e[36m###############################################\e[0m"
 echo -e "\e[1;33;4;44m Enter the name of the city you live or near city\e[0m"
 read town
-curl wttr.in/$town
+echo " ">report
+curl wttr.in/$town --connect-timeout 15 -o report -s
+count=$(< "report" wc -l)
+if [ $count -gt 2 ]
+then
+echo $'\n\n\n'
+head -n -1 report
+else
+echo -e "\e[1;31mConnection Timed Out..."
+echo -e "Seems That Your Input is Wrong or Net is Slow\e[0m"
+fi
+rm report >/dev/null 2>&1
+echo 
 echo -e "\e[34mAlso visit kalilinux.in for hacking tutorials\e[0m"
